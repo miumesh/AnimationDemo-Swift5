b1a36aa7dfc9348496f43cfb2405f723ee649d2a
Umesh <miumesh.k@gmail.com>
2019-03-04 23:06:26 +0530

    Initial commit with minor bugs

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by xyz on 24/02/19.
+//  Copyright © 2019 xyz. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIImageView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    
+    var player: AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+        }catch let error as NSError {
+            print(error.description)
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func powerBtnPressed(_ sender: Any) {
+        cloudHolder.isHidden = false
+        darkBlueBG.isHidden = true
+        powerBtn.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3,animations: {
+            self.rocket.frame = CGRect(x:0, y:20, width:412, height:574)
+        }){ (finished) in
+            self.hustleLbl.isHidden = false
+            self.onLbl.isHidden = false
+            
+        }
+    }
+}
+
